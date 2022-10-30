09c601d16b9941e5e62013a992bdb0fcd5442390
Roger Cristian <erogennin@gmail.com>
2022-10-30 14:03:17 -0300

    Calc arquivos

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+//factory function
+
+function criaCalculadora(){
+    return{
+        display: document.querySelector('.display'),
+
+        inicia(){
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e=>{
+                if(e.keyCode ===13){
+                    this.realizaConta();
+                }
+            });
+        },
+        realizaConta(){
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta){
+                    alert( 'conta invalida');
+                    return;
+                }
+
+                this.display.value=String(conta);
+            } catch (error) {
+                alert(' conta invalida');
+
+                return;
+            }
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0,-1);
+        },
+        
+        cliqueBotoes(){
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+                if(el.classList.contains('btn-clear')){
+                    this.display.value = '';  
+                }
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        },
+
+    };
+}
+
+const calculaora = criaCalculadora();
+calculaora.inicia();
